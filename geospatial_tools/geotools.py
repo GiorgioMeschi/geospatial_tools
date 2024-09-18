4e7a6fab8b4eece7b58c9ad2d625fa8667c34861
Gruppo 4 <gruppo4@fuoco>
2024-09-18 09:40:51 +0200

    add gdal_reproject_raster_as function using gdal

--- geospatial_tools/geotools.py
+++ geospatial_tools/geotools.py
@@ -82,28 +82,37 @@ class Raster:
             with rio.open(output_file, 'w', **profile) as dst:
                 dst.write(array)    
                 
-    # def reproject_raster_as(self, input_file: str, output_file: str, reference_file: str) -> str:
-    #     '''
-    #     reproj and clip raster based on reference file
-    #     '''
+    def gdal_reproject_raster_as(self, input_file: str, output_file: str, reference_file: str) -> str:
+        '''
+        reproj and clip raster based on reference file using gdal
+        '''
 
-    #     with rio.open(input_file) as file_i:
-    #         input_crs = file_i.crs
-    #         #bounds = haz.bounds
-    #     with rio.open(reference_file) as ref:
-    #         bounds = ref.bounds
-    #         res = ref.transform[0]
-    #         output_crs = ref.crs
+        try:
+            from osgeo import gdal
+        except ImportError:
+            import gdal
+
+        with rio.open(input_file) as file_i:
+            input_crs = file_i.crs
+            #bounds = haz.bounds
+        with rio.open(reference_file) as ref:
+            bounds = ref.bounds
+            res = ref.transform[0]
+            output_crs = ref.crs
     
-    #     gdal.Warp(output_file, input_file,
-    #                     outputBounds = bounds, xRes=res, yRes=res,
-    #                     srcSRS = input_crs, dstSRS = output_crs, dstNodata = -9999,
-    #                     creationOptions=["COMPRESS=LZW", "PREDICTOR=2", "ZLEVEL=3", "BLOCKXSIZE=512", "BLOCKYSIZE=512"])    
+        gdal.Warp(output_file, input_file,
+                        outputBounds = bounds, xRes=res, yRes=res,
+                        srcSRS = input_crs, dstSRS = output_crs, dstNodata = -9999,
+                        creationOptions=["COMPRESS=LZW", "PREDICTOR=2", "ZLEVEL=3", "BLOCKXSIZE=512", "BLOCKYSIZE=512"])    
         
-    #     return output_file
+        return output_file
 
     def reproject_raster_as(input_file: str, output_file: str, reference_file: str, method = Resampling.nearest) -> str:
         
+        '''
+        reproj and clip raster based on reference file using rasterio
+        '''
+
         with rio.open(reference_file) as ref:
             with rio.open(input_file) as src:       
                 kwargs = src.meta.copy()     
